15a53608b4ce1bea3456f6ce5601667f9aaaebd0
Yasmin Pereira <danieleyasminpoli@gmail.com>
2023-06-20 21:38:51 -0300

    edição
    editando formulario

--- site padaria/enviar.php
+++ site padaria/enviar.php
@@ -4,13 +4,13 @@ $nome = addslashes($_POST['nome']);
 $email = addslashes($_POST['email']);
 $telefone = addslashes($_POST['telefone']);
 
-$para = "igoritoaraujo223@gmail.com";
+$para = "danieleyasminpoli@gmail.com";
 $assunto = "Coleta de dados - Webdesigner"
 
 
 $corpo = "Nome: ".$nome."\n"."E-mail: ".$email."\n"."Telefone: ".$telefone;
 
-$cabeca = "From: igor.cartorioermelino@gmail.com"."\n"."Reply-to: ".$email."\n"."X=Mailer:PHP/".phpversion(); 
+$cabeca = "From: danieleyasminpoli@gmail.com"."\n"."Reply-to: ".$email."\n"."X=Mailer:PHP/".phpversion(); 
 
 if(mail($para,$assunto,$corpo,$cabeca)){
     echo("E-mail enviado com sucesso!")
